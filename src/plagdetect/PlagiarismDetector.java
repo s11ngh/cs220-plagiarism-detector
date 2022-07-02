682e79635708f8ff5a0f4805ea22b34ff41840e9
Ujjwal Singh <86053991+s11ngh@users.noreply.github.com>
2022-07-02 13:53:18 +0530

    Update PlagiarismDetector.java
    second push

--- src/plagdetect/PlagiarismDetector.java
+++ src/plagdetect/PlagiarismDetector.java
@@ -6,15 +6,21 @@ import java.util.Collection;
 import java.util.Map;
 
 public class PlagiarismDetector implements IPlagiarismDetector {
+	int sizeNgram;
+	//store ngrams
+	Map<String, HashSet<String>> ngram;
+	//answer to compare them
+	Map<String, HashSet<ArrayList<String>>> answer;
 	
 	public PlagiarismDetector(int n) {
 		// TODO implement this method
+		sizeNgram=n;
 	}
 	
 	@Override
 	public int getN() {
 		// TODO Auto-generated method stub
-		return n;
+		return sizeNgram;
 	}
 
 	@Override
@@ -38,26 +44,45 @@ public class PlagiarismDetector implements IPlagiarismDetector {
 	@Override
 	public Map<String, Map<String, Integer>> getResults() {
 		// TODO Auto-generated method stub
-		return result;
+		return answer;
 	}
 
 	@Override
 	public void readFile(File file) throws IOException {
 		// TODO Auto-generated method stub
 		// most of your work can happen in this method
-		
+		Map<String, Integer> map = new HashMap<>();
+		for(String gram: ngram.keySet()){
+			Set<String> add = new HashSet<>();
+			add = ngram.get(gram);
+			add.retainAll(ngram.getName());
+			map.put(gram, add.size());
+		}
+		answer.put(file.getName(), map);
 	}
 
 	@Override
 	public int getNumNGramsInCommon(String file1, String file2) {
 		// TODO Auto-generated method stub
-		return 0;
+		if(!answer.containsKey(file1) || !answer.containsKey(file2)){
+			return 0;
+		} else{return answer.get(file1).get(file2);}
 	}
 
 	@Override
 	public Collection<String> getSuspiciousPairs(int minNgrams) {
 		// TODO Auto-generated method stub
-		return null;
+		Collection<String> gram1 = answer.keySet();
+		Collection<String> gram2 = gram1;
+		Set<String> alleged = new HashSet<>();
+		for(String s: gram1){
+			for(String t: gram2){
+				if(answer.getNumGramsInCommon(s,t)>=minNgrams && 0 > s.compare()){
+					alleged.add(s+" "+t+" "+getNumGramsInCommon(s,t));
+				}
+			}
+		}
+		return alleged;
 	}
 
 	@Override
