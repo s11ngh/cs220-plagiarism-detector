c95bab3d7d67b36f060078c6e290acad51651937
Ujjwal Singh <86053991+s11ngh@users.noreply.github.com>
2022-07-02 14:04:05 +0530

    Update TestAllDocs.java
    changed getSuspiciousParirs(500) to 75

--- src/junit/TestAllDocs.java
+++ src/junit/TestAllDocs.java
@@ -18,13 +18,7 @@ class TestAllDocs {
     }
     
 	public static void main(String[] args) throws Exception {
-		//
-		// TODO you probably want to add more code or more tests
-		// This just gets things started
-		//
 		
-		
-		// how long does it take to create a detector?
 		long start = System.currentTimeMillis();
 		IPlagiarismDetector detector = makeDetector(3);
 		long total = System.currentTimeMillis() - start;
@@ -32,12 +26,8 @@ class TestAllDocs {
 		
 		// how long does it take to search for suspicious pairs?
 		start = System.currentTimeMillis();
-		// TODO: 500 is too big here! These are not long enough essays to
-		// have 500 ngrams in common!
-		// The average essay is less than 500 words long.
-		// What is a good number to put here? Try some different values to
-		// figure that out!
-		Collection<String> pairs = detector.getSuspiciousPairs(500);
+		
+		Collection<String> pairs = detector.getSuspiciousPairs(75);
 		for (String pair : pairs) {
 			System.out.println(pair);
 		}
